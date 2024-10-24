ea5ad231868d112c32d684c971de99043652b257
Alicia <117280585+alyrute@users.noreply.github.com>
2024-10-24 09:20:34 +0200

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,8 +1,7 @@
-// src/api.js
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000/', // Cambia esto a la URL de tu API
+  baseURL: 'http://192.168.1.33:8080/', // Cambia esto a la URL de tu API
   timeout: 10000, // Puedes ajustar el tiempo de espera si lo necesitas
 });
 
